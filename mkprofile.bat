566461ff46d4045ae5fae443e2d199dbcfe2ba9d
jAdameA <javieradameardila@gmail.com>
2024-11-18 10:05:35 +0100

    Add files via upload

---
+++ mkprofile.bat
@@ -0,0 +1,16 @@
+@echo off
+rem script de prueba
+rem Autor: Javier Adame
+rem Fecha: 18-11-2024
+
+mkdir %USERPROFILE%\asir %USERPROFILE%\daw %USERPROFILE%\dam %USERPROFILE%\ceti
+
+mkdir %USERPROFILE%\asir\docs %USERPROFILE%\asir\pract %USERPROFILE%\asir\examen
+
+mkdir %USERPROFILE%\daw\docs  %USERPROFILE%\daw\pract %USERPROFILE%\daw\examen
+
+mkdir %USERPROFILE%\dam\docs  %USERPROFILE%\dam\pract %USERPROFILE%\dam\examen
+
+mkdir %USERPROFILE%\smr\docs %USERPROFILE%\smr\pract  %USERPROFILE%\smr\examen
+
+mkdir %USERPROFILE%\ceti\docs %USERPROFILE%\ceti\pract  %USERPROFILE%\ceti\examen
